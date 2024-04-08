3fd90d2823011443396456b22f3222babac7eb25
truman <trumanjimothy@gmail.com>
2024-04-08 18:53:26 -0400

    Made a simple car list

--- views/MainWindow.py
+++ views/MainWindow.py
@@ -1,8 +1,9 @@
 import sys
+from PyQt6 import QtCore, QtGui, QtWidgets
 from PyQt6.QtCore import (Qt, pyqtSignal)
 from PyQt6.QtWidgets import (
     QWidget, QPushButton, QApplication, QGridLayout,
-    QLabel, )
+    QLabel, QScrollArea)
 
 
 from views.LogInWindow import LogInWindow
@@ -10,6 +11,7 @@ from views.SignUpWindow import SignUpWindow
 from views.SignUpWindow import screen_size
 from models.Accounts import Accounts
 from helpers.EnvVariables import EnvVariables
+from views.CarList import CarList
 
 class MainWindow(QWidget):
     # Basically the home page just a stand in
@@ -17,8 +19,11 @@ class MainWindow(QWidget):
         super().__init__()
 
         self.layout = QGridLayout()
-        self.layout.setContentsMargins(50, 50, 50, 50)
-        self.layout.setSpacing(10)
+        self.layout.setRowMinimumHeight(0, 50)
+        self.layout.setRowMinimumHeight(1, 50)
+        self.layout.setColumnMinimumWidth(0, int(screen_size.width() / 10))
+        self.layout.setColumnMinimumWidth(2, int(screen_size.width() / 3) * 2)
+        self.layout.setContentsMargins(50, 25, 50, 100)
 
         self.setWindowTitle("Home Page")
         self.setLayout(self.layout)
@@ -27,30 +32,47 @@ class MainWindow(QWidget):
 
         self.account = Accounts()
 
+        #Car collection list
+        self.cars = CarList()
+        self.car_list = self.cars.make_car_list()
+        self.layout.addWidget(self.car_list, 2, 2)
+        self.car_list.hide()
+
         # Welcomes to home page
-        welcome_label = QLabel("Welcome to the VehicleVenue")
-        welcome_label.setProperty("class", "heading")
-        self.layout.addWidget(welcome_label, 0, 0, 3, 0, Qt.AlignmentFlag.AlignCenter)
+        self.welcome_label = QLabel("Welcome to the VehicleVenue")
+        self.welcome_label.setProperty("class", "heading")
+        self.layout.addWidget(self.welcome_label, 2, 2, Qt.AlignmentFlag.AlignCenter)
 
         self.user_name_label = QLabel("Guest")
         self.user_name_label.setProperty("class", "heading")
         self.layout.addWidget(self.user_name_label, 0, 0, 0, 0, Qt.AlignmentFlag.AlignTop)
 
+        #Tab that goes to the car list
+        self.collection_tab = QPushButton("Car Collection")
+        self.collection_tab.setFlat(True)
+        self.collection_tab.clicked.connect(self.car_collection)
+        self.layout.addWidget(self.collection_tab, 1, 0, Qt.AlignmentFlag.AlignRight)
+
+        #Tab that goes to the home page
+        self.home_tab = QPushButton("Home")
+        self.home_tab.setFlat(True)
+        self.home_tab.clicked.connect(self.home_page)
+        self.layout.addWidget(self.home_tab, 1, 0, Qt.AlignmentFlag.AlignLeft)
 
         # Sign Up button
         sign_up_button = QPushButton("Sign Up")
         sign_up_button.clicked.connect(self.sign_up_window)
-        self.layout.addWidget(sign_up_button, 0, 3)
+        self.layout.addWidget(sign_up_button, 1, 3)
 
         # Log In button
         self.login_button = QPushButton("Log In")
         self.login_button.clicked.connect(self.login_window)
-        self.layout.addWidget(self.login_button, 0, 4)
+        self.layout.addWidget(self.login_button, 1, 4)
         
         # Log Out button
         self.logout_button = QPushButton("Log Out")
         self.logout_button.clicked.connect(self.logout)
-        self.layout.addWidget(self.logout_button, 0, 4)
+        self.layout.addWidget(self.logout_button, 1, 4)
 
 
         self. logout_button.hide()
@@ -60,7 +82,7 @@ class MainWindow(QWidget):
 
         self.login_window = LogInWindow()
         self.login_window.window_closed.connect(self.login_check)
-        
+
     def log_in_window(self):
         self.log_in_window = LogInWindow()
         self.log_in_window.show()
@@ -87,6 +109,14 @@ class MainWindow(QWidget):
             self.logout_button.show()
             self.login_button.hide()
             self.user_name_label.setText(env_vars.get_user())
+
+    def car_collection(self):
+        self.welcome_label.hide()
+        self.car_list.show()
+
+    def home_page(self):
+        self.car_list.hide()
+        self.welcome_label.show()
 
 
 app = QApplication(sys.argv)
