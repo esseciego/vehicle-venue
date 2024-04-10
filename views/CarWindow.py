883a801285e15f3fff12479abf86b17ff99a9dc4
reidacastillo <reidacastillo@ufl.edu>
2024-04-10 18:40:42 -0400

    Car updates

--- views/CarWindow.py
+++ views/CarWindow.py
@@ -1,14 +1,7 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QTableWidget, QTableWidgetItem, QPushButton
-from PyQt6.QtCore import Qt
-from pymongo import MongoClient
-from bson import ObjectId
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QTableWidget, QTableWidgetItem, QPushButton
-from PyQt6.QtCore import Qt
 from pymongo import MongoClient
 from bson import ObjectId
-
-
 
 class CarWindow(QWidget):
     def __init__(self):
@@ -16,54 +9,42 @@ class CarWindow(QWidget):
         self.initUI()
 
     def initUI(self):
-        layout = QVBoxLayout(self)
-
-        self.car_table = QTableWidget()
-        self.car_table.setColumnCount(5)  # Adjust the number based on car attributes
-        self.car_table.setHorizontalHeaderLabels(["Make", "Model", "Year", "Status", "Location"])
-        layout.addWidget(self.car_table)
-
-        self.refresh_button = QPushButton("Refresh Car List")
-        self.refresh_button.clicked.connect(self.load_cars)
-        layout.addWidget(self.refresh_button)
-
-        self.setLayout(layout)
+        # UI setup
         self.setWindowTitle("Car Management")
-        self.load_cars()
+        self.layout = QVBoxLayout(self)
+        self.table_widget = QTableWidget()
+        self.layout.addWidget(self.table_widget)
+        self.populate_table_button = QPushButton("Load Cars")
+        self.populate_table_button.clicked.connect(self.populate_table)
+        self.layout.addWidget(self.populate_table_button)
 
-    def load_cars(self):
-        # Connect to the database and fetch car data
-        client = MongoClient('your_connection_string')
-        db = client['car_rental_database']
+    def populate_table(self):
+        client = MongoClient('mongodb+srv://tears_user:sobbing.emoji@carrental.fiinqnj.mongodb.net/?retryWrites=true&w=majority&appName=CarRental')
+        db = client['car_rental_data']
         cars_collection = db['cars']
 
+        # Fetch cars data from MongoDB
         cars = list(cars_collection.find())
 
-        self.populate_table(cars)
+        # Set the table row count and column count
+        self.table_widget.setRowCount(len(cars))
+        self.table_widget.setColumnCount(7)  # Update this if you have more or fewer columns
 
-    def populate_table(self, cars):
-        self.car_table.setRowCount(len(cars))
+        # Set the table headers
+        self.table_widget.setHorizontalHeaderLabels([
+            "License Plate", "Type", "Current Rental Location",
+            "Mileage", "Cost Per Day", "Cost Per Mile", "Current Car Status"
+        ])
+
+        # Populate the table rows with car data
         for row, car in enumerate(cars):
-            self.car_table.setItem(row, 0, QTableWidgetItem(car.get('make', '')))
-            self.car_table.setItem(row, 1, QTableWidgetItem(car.get('model', '')))
-            self.car_table.setItem(row, 2, QTableWidgetItem(str(car.get('year', ''))))
-            self.car_table.setItem(row, 3, QTableWidgetItem(car.get('status', '')))
-            self.car_table.setItem(row, 4, QTableWidgetItem(car.get('location', '')))
-
-            # Store the car's ObjectId in the row for later reference
-            self.car_table.item(row, 0).setData(Qt.ItemDataRole.UserRole, str(car['_id']))
-
-        self.car_table.cellChanged.connect(self.on_cell_changed)
-
-    def on_cell_changed(self, row, column):
-        car_id_str = self.car_table.item(row, 0).data(Qt.ItemDataRole.UserRole)
-        new_value = self.car_table.item(row, column).text()
-
-        if car_id_str:
-            car_id = ObjectId(car_id_str)
-            # Update logic here
-            print(f"Updated car {car_id} in column {column} with new value: {new_value}")
-
+            self.table_widget.setItem(row, 0, QTableWidgetItem(car.get('license_plate', '')))
+            self.table_widget.setItem(row, 1, QTableWidgetItem(car.get('type', '')))
+            self.table_widget.setItem(row, 2, QTableWidgetItem(car.get('curr_rental_location', '')))
+            self.table_widget.setItem(row, 3, QTableWidgetItem(str(car.get('mileage', ''))))
+            self.table_widget.setItem(row, 4, QTableWidgetItem(str(car.get('cost_per_day', ''))))
+            self.table_widget.setItem(row, 5, QTableWidgetItem(str(car.get('cost_per_mile', ''))))
+            self.table_widget.setItem(row, 6, QTableWidgetItem(car.get('curr_car_status', '')))
 
 
 if __name__ == "__main__":
@@ -71,4 +52,3 @@ if __name__ == "__main__":
     window = CarWindow()
     window.show()
     sys.exit(app.exec())
-
