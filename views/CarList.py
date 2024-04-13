f34fb5c8368e957b4a1234c24e025aa89b91f5fc
truman <trumanjimothy@gmail.com>
2024-04-13 12:21:36 -0400

    car list and specifc car window

--- views/CarList.py
+++ views/CarList.py
@@ -16,16 +16,17 @@ class CarList(QWidget):
 
         self.window_layout = SpecificCarWindow()
 
-
         self.scroll = QScrollArea()  # Scroll Area which contains the widgets
         self.car_list = QWidget()  # Widget that contains the collection of the cars
         self.list_layout = QGridLayout() #Layout of the cars
+
         self.cars = Cars()
+
         self.list_of_cars = self.cars.show_all_cars()
 
     def make_car_list(self):
             for i in range(self.cars.get_num_cars()):
-                car_object = self.make_car_object(self.list_of_cars, i)
+                car_object = self.make_car_object(i)
                 self.list_layout.addWidget(car_object, int(i / 3), i % 3)
 
             self.car_list.setLayout(self.list_layout)
@@ -35,19 +36,34 @@ class CarList(QWidget):
             self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
             self.scroll.setWidgetResizable(True)
             self.scroll.setWidget(self.car_list)
-            self.scroll.setFixedSize(int(screen_size.width() / 3) * 2, int(screen_size.height() / 3) * 2)
+            self.scroll.setFixedSize(int(screen_size.width() / 3) * 2, int(screen_size.height() / 4) * 3)
 
             return self.scroll
 
-    def make_car_object(self, car_l, i):
-        car_object = QPushButton('')
-        icon = QtGui.QIcon('ui/images/car_icon.png')
-        car_object.setIcon(icon)
-        car_object.setIconSize(QtCore.QSize(200, 200))
-        car_object.setFixedSize(200, 200)
-        car_object.clicked.connect(lambda : self.CarWindow(car_object, i))
-        return car_object
+    def make_car_object(self, i):
+        car_button = QPushButton('')
+        if self.list_of_cars[i]['type'] == "SUV":
+            icon = QtGui.QIcon('ui/images/suv_icon.png')
+        elif self.list_of_cars[i]['type'] == "Van":
+            icon = QtGui.QIcon('ui/images/van_icon.png')
+        else:
+            icon = QtGui.QIcon('ui/images/sedan_icon.png')
+        car_button.setIcon(icon)
+        car_button.setIconSize(QtCore.QSize(200, 200))
+        car_button.setFixedSize(200, 200)
+        car_button.clicked.connect(lambda : self.car_window(i))
 
-    def CarWindow(self, car_object, i):
+        return car_button
+
+    def car_window(self, i):
+        self.window_layout.set_pixmap(self.list_of_cars[i]['type'])
+
+        self.window_layout.license_plate_label.setText("License plate number: " + self.list_of_cars[i]['license_plate'])
+        self.window_layout.type_label.setText("Type: " + self.list_of_cars[i]['type'])
+        self.window_layout.curr_rental_location_label.setText("Location: " + self.list_of_cars[i]['curr_rental_location'])
+        self.window_layout.mileage_label.setText("Mileage: " + self.list_of_cars[i]['mileage'])
+        self.window_layout.cost_per_day_label.setText("Cost Per Day: " + self.list_of_cars[i]['cost_per_day'])
+        self.window_layout.cost_per_mile_label.setText("Cost Per Mile: " + self.list_of_cars[i]['cost_per_mile'])
+
         self.window_layout.show()
 
